d25c2356823a0e1c991c0d95eff3228a0c42e799
Gois <matheusgoislimasilva@gmail.com>
2021-11-08 23:38:18 -0300

    improve tests

--- Tests/AwakeTests/AwakeTests.swift
+++ Tests/AwakeTests/AwakeTests.swift
@@ -4,7 +4,6 @@ import XCTest
 final class AwakeTests: XCTestCase {
     func wakeMyMachine() {
         // IMPORTANT! replace mac addr with yours, and in terminal run this command 'nc -ul 9'
-        let device = Awake.Device(MAC: "18:c0:4d:a2:ba:8a", broadcastAddr: "255.255.255.255")
-        _ = Awake.target(device: device)
+        Awake.target(device: .init(MAC: "18:c0:4d:a2:ba:8a", broadcastAddr: "255.255.255.255"))
     }
 }
